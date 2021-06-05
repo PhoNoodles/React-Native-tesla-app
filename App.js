af2758e684da0a3e924c0734e7508e567de0bda0
Rio <riokimle123@gmail.com>
2021-06-04 17:01:00 -0700

    made a car component

--- App.js
+++ App.js
@@ -1,17 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import CarItem from "./components/CarItem/index";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.carContainer}>
-        <View style={styles.titles}>
-          <Text style={styles.title}>Model S</Text>
-          <Text style={styles.subtitle}>Starting at $69,000</Text>
-        </View>
-      </View>
-
+      <CarItem />
       <StatusBar style="auto" />
     </View>
   );
@@ -23,22 +18,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-  },
-  carContainer: {
-    width: "100%",
-    height: "100%",
-  },
-  titles: {
-    marginTop: "30%",
-    width: "100%",
-    alignItems: "center",
-  },
-  title: {
-    fontSize: 40,
-    fontWeight: "600",
-  },
-  subtitle: {
-    fontSize: 16,
-    color: "#5c5e62",
   },
 });
